2ecd7d701b40121d05922ef1f4dc0dcd29e6203b
gisegind <gindelligisela@gmail.com>
2024-12-27 23:12:42 -0300

    Creo mi portfolio. Utilizo framework tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export const content = [];
+export const theme = {
+    extend: {
+        fontFamily: {
+            sans: ['Poppins', 'sans-serif'], // Agregas Poppins como fuente sans
+        },
+    },
+};
+export const variants = {};
+export const plugins = [];
